a7ea9fceaa32f209901efb28bcfe13c68d02d391
Darklg <kev.rocher@gmail.com>
2011-08-10 15:37:05 +0200

    Léger style du script & sélecteur par défaut

--- index.php
+++ index.php
@@ -7,13 +7,15 @@ include dirname(__FILE__).'/inc/upload.php';
 <html lang="fr">
     <head>
         <meta charset="utf-8"/>
-        <title>untitled</title>
+        <title>IMG.2.DATA:URI</title>
+		<link rel="stylesheet" href="css/style.css" type="text/css" />
     </head>
     <body>
+		<h1>IMG.2.DATA:URI</h1>
         <?php echo $retour; ?>
         <form method="post" enctype="multipart/form-data" action="">
             <p>
-                <label>Selecteur : <input type="text" name="selecteur" size="30" /></label><br />
+                <label>Selecteur : <input type="text" name="selecteur" size="30" value=".maclass" /></label><br />
                 <input type="file" name="fichier" size="30" /><br />
                 <input type="submit" name="upload" value="Uploader" />
             </p>
